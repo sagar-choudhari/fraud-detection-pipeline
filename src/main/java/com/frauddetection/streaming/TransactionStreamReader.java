a9689568f55d13b83e814074e71f5d3f085d69de
Sagar <sgrchdhr82@gmail.com>
2026-07-12 22:00:46 +0530

    Spark Structured Streaming parsing Kafka JSON into structured DataFrame with checkpointing

--- src/main/java/com/frauddetection/streaming/TransactionStreamReader.java
+++ src/main/java/com/frauddetection/streaming/TransactionStreamReader.java
@@ -5,18 +5,27 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 
 import java.util.concurrent.TimeoutException;
-
+import static org.apache.spark.sql.functions.*;
 public class TransactionStreamReader {
 
     public static void main(String[] args) throws TimeoutException, StreamingQueryException {
         SparkSession spark = SparkSession.builder()
-                .appName("TransactionStreaming")
+                .appName("Transaction Stream Reader")
                 .master("local[*]")
                 .getOrCreate();
 
         spark.sparkContext().setLogLevel("ERROR");
+
+        StructType transactionSchema = new StructType()
+                .add("txnId",     DataTypes.StringType,  false)
+                .add("userId",    DataTypes.StringType,  false)
+                .add("amount",    DataTypes.DoubleType,  false)
+                .add("merchant",  DataTypes.StringType,  false)
+                .add("timestamp", DataTypes.StringType,  false);
 
         Dataset<Row> kafkaStream = spark.readStream()
                 .format("kafka")
@@ -25,11 +34,20 @@ public class TransactionStreamReader {
                 .option("startingOffsets", "earliest")
                 .load();
 
-        kafkaStream.printSchema();
+        Dataset<Row> transactions = kafkaStream
+                .selectExpr("CAST(value AS STRING) as jsonStr")
+                .select(
+                        from_json(col("jsonStr"), transactionSchema).alias("data")
+                )
+                .select("data.*");
 
-        StreamingQuery query = kafkaStream.writeStream()
+        transactions.printSchema();
+
+        StreamingQuery query = transactions.writeStream()
                 .format("console")
                 .outputMode("append")
+                .option("checkpointLocation", "checkpoint/transactions")
+                .option("truncate", false)
                 .start();
 
         query.awaitTermination();
